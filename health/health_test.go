e4a418fd42ccc6744af886a50488089838c09bda
lnwsitgod <sittichai.ansap@gmail.com>
2023-01-06 01:30:43 +0700

    refactor name and assert NoError after new request

--- health/health_test.go
+++ health/health_test.go
@@ -11,20 +11,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHealthHandler(t *testing.T) {
-
+func TestGetHealthHandler(t *testing.T) {
 	req, err := http.NewRequest(http.MethodGet, "/health", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	assert.NoError(t, err)
 
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := echo.New().NewContext(req, rec)
 
-	resperr := HealthHandler(c)
+	err = GetHealthHandler(c)
 
-	if assert.NoError(t, resperr) {
+	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, "healthy", rec.Body.String())
 	}
