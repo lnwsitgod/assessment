ac75367536bf1a336ad8b875bbe22ffa41125164
lnwsitgod <sittichai.ansap@gmail.com>
2023-01-06 01:31:00 +0700

    rename

--- health/health.go
+++ health/health.go
@@ -6,6 +6,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func HealthHandler(c echo.Context) error {
+func GetHealthHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "healthy")
 }
