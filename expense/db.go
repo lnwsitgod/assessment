2b6da35b82a4010978519a71960d3b4c356e2f62
lnwsitgod <sittichai.ansap@gmail.com>
2023-01-06 16:09:15 +0700

    create db.go

---
+++ expense/db.go
@@ -0,0 +1,33 @@
+package expense
+
+import (
+	"database/sql"
+	"log"
+	"os"
+)
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open(os.Getenv("DATABASE_DRIVER"), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);
+	`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+}
